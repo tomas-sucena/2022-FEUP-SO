9ffa160357480e79de22345d04ae673dd4e9c8b8
TomA!s de Campos Sucena de Sequeiros Lopes <tomas21sucena@gmail.com>
2022-11-30 12:36:15 +0000

    SO more like SOffering

--- G1PL08/Q2/txt2epub.c
+++ G1PL08/Q2/txt2epub.c
@@ -1,55 +1,81 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-int main(int argc, char* argv[]){
+// função auxiliar usada para converter os ficheiros '.txt' para '.epub'
+int convert_to_epub(char* txt, char* epub){
+    char* command[1024] = {"pandoc", txt, "-o", epub, NULL};
 
-    pid_t pid;
+    // converter o ficheiro para '.epub'
+    if (execvp("pandoc", command) < 0){
+        char* error_msg = (char*) malloc(64 * sizeof(char));
+        sprintf(error_msg, "Error! Could not convert %s to %s", txt, epub);
 
-    char* lastcommand[1024] = {"zip", "ebooks.zip"}; // cria o comando zip
+        perror(error_msg);
 
-    // cria um processo child por cada ficheiro por converter
-    for(int i = 1; i < argc; i++){
-
-        char *filename = (char*)malloc(sizeof(argv[i])-4);
-
-        strcpy(filename,argv[i]);
-
-        filename[strlen(argv[i])-4] = '\0'; // elimina a extensão '.txt' do ficheiro
-
-        char *epub = (char*)malloc(sizeof(argv[i])+1);
-
-        strcpy(epub,filename);
-
-        strcat(epub,".epub"); // adiciona a extersão '.epub'
-
-        lastcommand[i+1] = epub; // adiciona o ficheiro ao comando zip
-
-        // cria o processo
-        if ((pid = fork()) < 0) { // error
-            perror("fork error");
-            exit(EXIT_FAILURE);
-        }   
-        else if (pid > 0){
-            /* Parent */
-            if (waitpid(pid, NULL, 0) < 0){ // à espera do child
-                fprintf(stderr, "Cannot wait for child: %s\n", strerror(errno));
-            }
-            printf("[pid%d] converting %s ...\n", pid, argv[i]);
-        } 
-        else {
-            /* Child */
-            char* command[1024] = {"pandoc", argv[i], "-o", epub, NULL};
-            execvp("pandoc", command); // pandoc command
-            return EXIT_SUCCESS;
-        }
-    }
-    if(execvp("zip",lastcommand) == -1){ // zip command
-        perror("Could not Zip");
         return EXIT_FAILURE;
     }
+
+    return EXIT_SUCCESS;
+}
+
+int main(int argc, char* argv[]){
+    // verificar se há argumentos suficientes
+    if (argc < 2){
+        perror("Error! Not enough arguments");
+        return EXIT_FAILURE;
+    }
+
+    char* lastcommand[1024] = {"zip", "ebooks.zip"}; // comando zip
+
+    // criar os processos
+    for(int i = 1; i < argc; i++){
+        char* filename = (char*) malloc(sizeof(argv[i]));
+
+        // eliminar a extensão '.txt' do ficheiro
+        strcpy(filename, argv[i]);
+        filename[strlen(filename) - 4] = '\0'; 
+
+        // adicionar a extensão '.epub'
+        char *epub = (char*) malloc(sizeof(argv[i]) + 1);
+        strcpy(epub, filename);
+        strcat(epub,".epub"); 
+
+        // adicionar o ficheiro ao comando zip
+        lastcommand[i + 1] = epub; 
+
+        pid_t pid = fork();
+
+        if (pid < 0){ // error
+            perror("fork error");
+            return EXIT_FAILURE;
+        }   
+        else if (pid == 0){
+            /* Child */
+            return convert_to_epub(argv[i], epub);
+        }
+        else{
+            /* Parent */
+            printf("[pid%d] converting %s ...\n", pid, argv[i]);
+        }
+    }
+
+    // esperar que os processos child terminem a conversão
+    for (int i = 1; i < argc; i++) {
+        if (waitpid(-1, NULL, 0) < 0){
+            perror("Error! Could not wait for children processes");
+            return EXIT_FAILURE;
+        }
+    }
+
+    // criar o ficheiro '.zip'
+    if (execvp("zip", lastcommand) < 0){
+        perror("Error! Could not Zip");
+        return EXIT_FAILURE;
+    }
+
     return EXIT_SUCCESS;
 }
