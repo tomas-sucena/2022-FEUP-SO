03ae7ce17f36895d80d462f8cb1d504c3ab62652
tomas-sucena <tomas21sucena@gmail.com>
2022-12-01 10:42:12 +0000

    trying

--- G1PL08/Q3/tokenring.c
+++ G1PL08/Q3/tokenring.c
@@ -9,11 +9,27 @@
 #define READ 0
 #define WRITE 1
 
-static volatile int running = 1;
+/* VARIÁVEIS GLOBAIS */
+static volatile sig_atomic_t running = 1;
+int n; // número de processos
+int MAX_PIPENAME_SIZE;
 
 // função auxiliar usada para interromper o ciclo infinito
-void sig_handler(int n){
+static void sig_handler(int _){
+    (void) _;
+    puts("Hey\n");
     running = 0;
+
+    char* pipename = (char*) malloc(MAX_PIPENAME_SIZE * sizeof(char));
+
+    // eliminar os named pipes
+    for (int i = 1; i <= n; i++){
+        int next = (i == n) ?  1 : i + 1;
+        
+        sprintf(pipename, "pipes/pipe%dto%d", i, next);
+
+        unlink(pipename);
+    }
 }
 
 // função auxiliar usada para descobrir o nº de dígitos de um inteiro
@@ -34,7 +50,7 @@ int main(int argc, char* argv[]){
     }
 
     /* ARGUMENTOS */
-    int n = atoi(argv[1]);
+    n = atoi(argv[1]);
     float p = atof(argv[2]);
     float t = atof(argv[3]);
 
@@ -53,7 +69,7 @@ int main(int argc, char* argv[]){
     }
 
     int fd[2]; // file descriptor
-    int MAX_PIPENAME_SIZE = 12 + 2 * int_digits(n);
+    MAX_PIPENAME_SIZE = 12 + 2 * int_digits(n);
     char* pipename = (char*) malloc(MAX_PIPENAME_SIZE * sizeof(char));
 
     // criar os named pipes
@@ -139,10 +155,4 @@ w:      if (p >= rng()){
         write(fd[WRITE], &token, sizeof(token));
         close(fd[WRITE]);
     }
-
-    // eliminar os named pipes        
-    sprintf(pipename, "pipes/pipe%dto%d", p_num, next);
-    unlink(pipename);
-    
-    return EXIT_SUCCESS;
 }
